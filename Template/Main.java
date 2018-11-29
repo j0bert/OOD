4df80e118a587414f492ac3835b84e4c535a2a01
Joel Snider <38743987+j0bert@users.noreply.github.com>
2018-11-29 18:21:35 -0500

    Create Main.java

---
+++ Template/Main.java
@@ -0,0 +1,17 @@
+package template;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Panini panini1 = new Panini();
+		panini1.makeSandwich();
+		
+		System.out.println();
+		
+		PattyMelt pattyMelt1 = new PattyMelt();
+		pattyMelt1.makeSandwich();
+		
+	}
+
+}
